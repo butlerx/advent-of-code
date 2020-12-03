8facb42241ff532c5a53f76569fed2fcf7387475
butlerx <butlerx@notthe.cloud>
2020-12-03 06:31:30 +0000

    day three solution

--- 2020/src/day_three.rs
+++ 2020/src/day_three.rs
@@ -1,17 +1,61 @@
 use std::io::Error;
 
-pub fn run(input: String) -> Result<i64, Error> {
-    Ok(0)
+pub fn run(input: String, right: i64, down: i64) -> Result<i64, Error> {
+    let tree = '#';
+    let (num_tree, _right_count) =
+        input
+            .lines()
+            .step_by(down as usize)
+            .fold((0, 0), |(num_tree, right_count), line| {
+                let mut chars = line.chars();
+                let len = chars.clone().collect::<Vec<_>>().len();
+                (
+                    num_tree
+                        + if chars.nth(right_count).unwrap() == tree {
+                            1
+                        } else {
+                            0
+                        },
+                    (right_count + right as usize) % len,
+                )
+            });
+    Ok(num_tree)
+}
+
+pub fn run_multiply(input: String) -> Result<i64, Error> {
+    Ok(run(input.clone(), 1, 1).unwrap()
+        * run(input.clone(), 3, 1).unwrap()
+        * run(input.clone(), 5, 1).unwrap()
+        * run(input.clone(), 7, 1).unwrap()
+        * run(input.clone(), 1, 2).unwrap())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    static INPUT: &str = "..##.........##.........##.........##.........##.........##.......
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#";
+
+    #[test]
+    fn test_run_multiply() {
+        assert!(run_multiply(INPUT.to_string()).unwrap() == 336);
+    }
 
     #[test]
     fn test_run() {
-        let input = "".to_string();
-        let results = run(input).unwrap();
-        assert!(results == 0);
+        assert!(run(INPUT.to_string(), 1, 1).unwrap() == 2);
+        assert!(run(INPUT.to_string(), 3, 1).unwrap() == 7);
+        assert!(run(INPUT.to_string(), 5, 1).unwrap() == 3);
+        assert!(run(INPUT.to_string(), 7, 1).unwrap() == 4);
+        assert!(run(INPUT.to_string(), 1, 2).unwrap() == 2);
     }
 }
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -31,6 +31,13 @@ enum Opt {
         /// Input File
         #[structopt(default_value = "day_three.txt", long)]
         input: String,
+        #[structopt(short, long)]
+        right: i64,
+        #[structopt(short, long)]
+        down: i64,
+        /// Flag for part 2
+        #[structopt(long)]
+        part_two: bool,
     },
 }
 
@@ -44,9 +51,18 @@ fn main() {
             let day_two_input = input::read(input).unwrap();
             day_two::run(day_two_input, position).unwrap()
         }
-        Opt::DayThree { input } => {
+        Opt::DayThree {
+            input,
+            right,
+            down,
+            part_two,
+        } => {
             let day_three_input = input::read(input).unwrap();
-            day_three::run(day_three_input).unwrap()
+            if !part_two {
+                day_three::run(day_three_input, right, down).unwrap()
+            } else {
+                day_three::run_multiply(day_three_input).unwrap()
+            }
         }
     };
     println!("results: {}", result);
