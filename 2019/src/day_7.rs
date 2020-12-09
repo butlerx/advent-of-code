7fad75dca8d2c68f66d4385e5c4356c86a309f3c
butlerx <butlerx@notthe.cloud>
2020-12-09 21:11:16 +0000

    2019: day 7 solution

--- 2019/src/day_7.rs
+++ 2019/src/day_7.rs
@@ -1,106 +1,131 @@
 use itertools::Itertools;
 
-// parse opscode, returning the code and if the args are in position mode or imediate
-// if an arg is true its in  imediate mode
-fn parse_opcode(code: i64) -> usize {
-    code as usize % 100
+#[derive(Clone)]
+struct Computer {
+    memory: Vec<i64>,
+    pointer: usize,
+    output: i64,
 }
 
-fn read_mem(memory: &Vec<i64>, pointer: usize, mem: usize) -> i64 {
-    if (memory[pointer] / 10i64.pow(mem as u32 + 1)) % 10 == 1 {
-        memory[pointer + mem]
-    } else {
-        memory[memory[pointer + mem] as usize]
+impl Computer {
+    fn new(input: &str) -> Self {
+        Computer {
+            memory: input
+                .split(",")
+                .map(|num| num.trim().parse().unwrap())
+                .collect(),
+            pointer: 0,
+            output: 0,
+        }
     }
-}
 
-fn execute(memory: Vec<i64>, input: Vec<i64>) -> i64 {
-    let mut mem = memory.clone();
-    let mut pointer = 0;
-    let mut output = 0;
-    let mut input = input.iter();
-    loop {
-        pointer = match parse_opcode(mem[pointer]) {
-            1 => {
-                let pos = mem[pointer + 3] as usize;
-                mem[pos] = read_mem(&mem, pointer, 1) + read_mem(&mem, pointer, 2);
-                pointer + 4
-            }
-            2 => {
-                let pos = mem[pointer + 3] as usize;
-                mem[pos] = read_mem(&mem, pointer, 1) * read_mem(&mem, pointer, 2);
-                pointer + 4
-            }
-            3 => {
-                let pos = mem[pointer + 1] as usize;
-                mem[pos] = *input.next().unwrap();
-                pointer + 2
-            }
-            4 => {
-                output = read_mem(&mem, pointer, 1);
-                // println!("Output: {}", output);
-                pointer + 2
-            }
-            5 => {
-                if read_mem(&mem, pointer, 1) != 0 {
-                    read_mem(&mem, pointer, 2) as usize
-                } else {
-                    pointer + 3
+    // parse opscode, returning the code and if the args are in position mode or imediate
+    // if an arg is true its in  imediate mode
+    fn parse_opcode(&self) -> usize {
+        self.memory[self.pointer] as usize % 100
+    }
+
+    fn read(&self, mem: usize) -> i64 {
+        if (self.memory[self.pointer] / 10i64.pow(mem as u32 + 1)) % 10 == 1 {
+            self.memory[self.pointer + mem]
+        } else {
+            self.memory[self.memory[self.pointer + mem] as usize]
+        }
+    }
+
+    fn read_pos(&self, pos: usize) -> usize {
+        self.memory[self.pointer + pos] as usize
+    }
+
+    fn execute(&mut self, input: Vec<i64>) -> Option<i64> {
+        let mut input = input.iter();
+        loop {
+            match self.parse_opcode() {
+                1 => {
+                    let pos = self.read_pos(3);
+                    self.memory[pos] = self.read(1) + self.read(2);
+                    self.pointer += 4;
                 }
-            }
-            6 => {
-                if read_mem(&mem, pointer, 1) == 0 {
-                    read_mem(&mem, pointer, 2) as usize
-                } else {
-                    pointer + 3
+                2 => {
+                    let pos = self.read_pos(3);
+                    self.memory[pos] = self.read(1) * self.read(2);
+                    self.pointer += 4;
                 }
+                3 => {
+                    let pos = self.read_pos(1);
+                    self.memory[pos] = *input.next().unwrap();
+                    self.pointer += 2;
+                }
+                4 => {
+                    self.output = self.read(1);
+                    // println!("Output: {}", output);
+                    self.pointer += 2;
+                    break Some(self.output);
+                }
+                5 => {
+                    if self.read(1) != 0 {
+                        self.pointer = self.read(2) as usize;
+                    } else {
+                        self.pointer += 3;
+                    }
+                }
+                6 => {
+                    if self.read(1) == 0 {
+                        self.pointer = self.read(2) as usize;
+                    } else {
+                        self.pointer += 3;
+                    }
+                }
+                7 => {
+                    let pos = self.read_pos(3);
+                    self.memory[pos] = if self.read(1) < self.read(2) { 1 } else { 0 };
+                    self.pointer += 4;
+                }
+                8 => {
+                    let pos = self.read_pos(3);
+                    self.memory[pos] = if self.read(1) == self.read(2) { 1 } else { 0 };
+                    self.pointer += 4;
+                }
+                99 => break None,
+                _ => unreachable!(),
             }
-            7 => {
-                let pos = mem[pointer + 3] as usize;
-                mem[pos] = if read_mem(&mem, pointer, 1) < read_mem(&mem, pointer, 2) {
-                    1
-                } else {
-                    0
-                };
-                pointer + 4
-            }
-            8 => {
-                let pos = mem[pointer + 3] as usize;
-                mem[pos] = if read_mem(&mem, pointer, 1) == read_mem(&mem, pointer, 2) {
-                    1
-                } else {
-                    0
-                };
-                pointer + 4
-            }
-            99 => break output as i64,
-            _ => unreachable!(),
         }
     }
 }
 
-fn applify(memory: &Vec<i64>, phases: &Vec<i64>) -> i64 {
-    println!("{:?}", phases);
+fn feedback(comp: &Computer, phases: &Vec<i64>) -> i64 {
+    let mut amplifiers: Vec<_> = phases.iter().map(|_| comp.clone()).collect();
+
+    let init = phases
+        .iter()
+        .zip(&mut amplifiers)
+        .fold(0, |input, (p, a)| a.execute(vec![*p, input]).unwrap());
+
+    (0..amplifiers.len())
+        .cycle()
+        .try_fold(init, |input, i| amplifiers[i].execute(vec![input]));
+
+    amplifiers.last().unwrap().output
+}
+
+fn applify(comp: &Computer, phases: &Vec<i64>) -> i64 {
     phases.iter().fold(0, |input, phase| {
-        execute(memory.clone(), vec![*phase, input])
+        comp.clone().execute(vec![*phase, input]).unwrap()
     })
 }
 
 pub fn run(input: &str, part_two: bool) -> i64 {
-    let code: Vec<i64> = input
-        .split(",")
-        .map(|num| num.trim().parse().unwrap())
-        .collect();
+    let comp = Computer::new(input);
     if part_two {
         (5..10)
             .permutations(5)
-            .map(|args| applify(&code, &args))
+            .map(|args| feedback(&comp, &args))
             .max()
             .unwrap_or(0)
     } else {
         (0..5)
             .permutations(5)
-            .map(|args| applify(&code, &args))
+            .map(|args| applify(&comp, &args))
             .max()
             .unwrap()
     }
@@ -136,6 +161,6 @@ mod tests {
         assert!(run(INPUT_5, true) == 18216);
         let results = run(include_str!("../input/day_7.txt"), true);
         println!("{}", results);
-        assert!(results == 0);
+        assert!(results == 1518124);
     }
 }
