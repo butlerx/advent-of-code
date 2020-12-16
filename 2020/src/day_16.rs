f454d72d213d6d9d72c48f5fd6abc7fe1d6398d9
butlerx <butlerx@notthe.cloud>
2020-12-16 12:19:22 +0000

    2020: day 16 hide mut while loops in a function

--- 2020/src/day_16.rs
+++ 2020/src/day_16.rs
@@ -4,30 +4,29 @@ use std::{
     ops::RangeInclusive,
 };
 
-#[derive(Debug)]
-struct Rule {
-    name: String,
-    left: RangeInclusive<i64>,
-    right: RangeInclusive<i64>,
-}
-type Schema = Vec<Rule>;
+type Rule = (RangeInclusive<i64>, RangeInclusive<i64>);
+type Schema = HashMap<String, Rule>;
+type Ticket = Vec<i64>;
 
 fn parse_schema(input: &str) -> Schema {
     let re = Regex::new(r"^([a-zA-Z\s]+): (\d+)-(\d+) or (\d+)-(\d+)").unwrap();
     input
+        .trim()
         .lines()
         .map(|line| {
             let cap = re.captures(line).unwrap();
-            Rule {
-                name: cap[1].trim().to_string(),
-                left: RangeInclusive::new(cap[2].parse().unwrap(), cap[3].parse().unwrap()),
-                right: RangeInclusive::new(cap[4].parse().unwrap(), cap[5].parse().unwrap()),
-            }
+            (
+                cap[1].trim().to_string(),
+                (
+                    RangeInclusive::new(cap[2].parse().unwrap(), cap[3].parse().unwrap()),
+                    RangeInclusive::new(cap[4].parse().unwrap(), cap[5].parse().unwrap()),
+                ),
+            )
         })
         .collect::<Schema>()
 }
 
-fn parse_tickets(input: &str) -> Vec<Vec<i64>> {
+fn parse_tickets(input: &str) -> Vec<Ticket> {
     input
         .trim()
         .lines()
@@ -41,36 +40,18 @@ fn parse_tickets(input: &str) -> Vec<Vec<i64>> {
         .collect()
 }
 
-fn invalid_fields(schema: &Schema, ticket: &Vec<i64>) -> i64 {
+fn invalid_fields(schema: &Schema, ticket: &Ticket) -> i64 {
     ticket
         .iter()
-        .filter(|value| !schema.iter().any(|ranges| valid_rule(ranges, value)))
+        .filter(|value| !schema.values().any(|ranges| valid_rule(ranges, value)))
         .sum()
 }
 
 fn valid_rule(ranges: &Rule, value: &i64) -> bool {
-    ranges.left.contains(&value) || ranges.right.contains(&value)
+    ranges.0.contains(&value) || ranges.1.contains(&value)
 }
 
-fn departure_data(schema: Schema, nearby_tickets: Vec<Vec<i64>>, your_ticket: Vec<i64>) -> i64 {
-    let valid_tickets: Vec<_> = nearby_tickets
-        .iter()
-        .filter(|ticket| invalid_fields(&schema, ticket) == 0)
-        .collect();
-    let mut map: HashMap<String, HashSet<usize>> = schema
-        .iter()
-        .map(|rule| {
-            let valid_options: HashSet<usize> = (0..schema.len())
-                .filter(|field_index| {
-                    valid_tickets
-                        .iter()
-                        .map(|ticket| ticket[*field_index])
-                        .all(|field| valid_rule(rule, &field))
-                })
-                .collect();
-            (rule.name.clone(), valid_options)
-        })
-        .collect();
+fn generate_name_index(mut map: HashMap<String, HashSet<usize>>) -> HashMap<String, usize> {
     let mut name_index = HashMap::new();
     while map.len() > 0 {
         for (k, v) in map.iter().filter(|(_, v)| v.len() == 1) {
@@ -85,17 +66,39 @@ fn departure_data(schema: Schema, nearby_tickets: Vec<Vec<i64>>, your_ticket: Ve
             }
         }
     }
-
     name_index
+}
+
+fn departure_data(schema: Schema, nearby_tickets: Vec<Vec<i64>>, your_ticket: Ticket) -> i64 {
+    let valid_tickets: Vec<_> = nearby_tickets
         .iter()
-        .filter(|(name, _)| name.starts_with("departure"))
-        .map(|(_, index)| your_ticket[*index])
-        .product()
+        .filter(|ticket| invalid_fields(&schema, ticket) == 0)
+        .collect();
+    generate_name_index(
+        schema
+            .iter()
+            .map(|(name, rule)| {
+                let valid_options: HashSet<usize> = (0..schema.len())
+                    .filter(|field_index| {
+                        valid_tickets
+                            .iter()
+                            .map(|ticket| ticket[*field_index])
+                            .all(|field| valid_rule(rule, &field))
+                    })
+                    .collect();
+                (name.clone(), valid_options)
+            })
+            .collect(),
+    )
+    .iter()
+    .filter(|(name, _)| name.starts_with("departure"))
+    .map(|(_, index)| your_ticket[*index])
+    .product()
 }
 
 pub fn run(input: &str, part_two: bool) -> i64 {
     let data: Vec<&str> = input.trim().split("\n\n").collect();
-    let schema = parse_schema(data[0].trim());
+    let schema = parse_schema(data[0]);
     let nearby_tickets = parse_tickets(data[2]);
     if part_two {
         let your_ticket = parse_tickets(data[1]);
