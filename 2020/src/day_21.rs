e64a3f9cc8740a433b4032728d423ea2870c8f10
butlerx <butlerx@notthe.cloud>
2020-12-22 09:25:43 +0000

    2020 day 21 and 22 solution

--- 2020/src/day_21.rs
+++ 2020/src/day_21.rs
@@ -1,29 +1,115 @@
-pub fn run(_input: &str, part_two: bool) -> i64 {
+use std::collections::{hash_map::Entry, HashMap, HashSet};
+
+type Allergens = HashMap<String, HashSet<String>>;
+type Ingredients = (Allergens, HashMap<String, usize>);
+
+fn parse_input(input: &str) -> Ingredients {
+    let mut allergen_map = HashMap::<String, HashSet<String>>::new();
+    let mut occurrences = HashMap::<String, usize>::new();
+    input.lines().for_each(|line| {
+        let contains = line.find(" (contains ").unwrap();
+        let ingredients = line[..contains]
+            .split(' ')
+            .map(String::from)
+            .collect::<HashSet<_>>();
+        let allergens = line[(contains + 11)..(line.len() - 1)]
+            .split(", ")
+            .map(String::from)
+            .collect::<Vec<_>>();
+        for allergen in allergens.iter() {
+            match allergen_map.entry(allergen.clone()) {
+                Entry::Vacant(entry) => {
+                    entry.insert(ingredients.clone());
+                }
+                Entry::Occupied(mut entry) => {
+                    *entry.get_mut() = entry
+                        .get()
+                        .intersection(&ingredients)
+                        .map(|i| i.clone())
+                        .collect();
+                }
+            }
+        }
+
+        for ingredient in ingredients.iter() {
+            *occurrences.entry(ingredient.clone()).or_insert(0) += 1;
+        }
+    });
+    for ingredients in allergen_map.values() {
+        for ingredient in ingredients.iter() {
+            occurrences.remove(ingredient);
+        }
+    }
+
+    (allergen_map, occurrences)
+}
+
+fn find_danderous_ingrediants(mut allergens: Allergens) -> Vec<String> {
+    let mut danger_list = Vec::<(String, String)>::new();
+    loop {
+        if allergens.is_empty() {
+            danger_list.sort_by_key(|(allergen, _)| allergen.clone());
+            break danger_list
+                .iter()
+                .map(|(_, ingredient)| ingredient.clone())
+                .collect();
+        }
+        for (allergen, ingredient_set) in allergens.iter() {
+            if ingredient_set.len() == 1 {
+                let ingredient = ingredient_set.iter().next().unwrap();
+                danger_list.push((allergen.clone(), ingredient.clone()));
+            }
+        }
+
+        danger_list.iter().for_each(|(allergen, _)| {
+            allergens.remove(allergen);
+        });
+
+        allergens.iter_mut().for_each(|(_, ingredient_set)| {
+            danger_list.iter().for_each(|(_, ingredient)| {
+                ingredient_set.remove(ingredient);
+            });
+        });
+    }
+}
+
+pub fn run(input: &str, part_two: bool) -> i64 {
+    let (allergens, occurrences) = parse_input(input);
     if part_two {
+        println!("{}", find_danderous_ingrediants(allergens).join(","));
         0
     } else {
-        0
+        occurrences
+            .values()
+            .fold(0, |sum, occurrence| sum + *occurrence as i64)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)";
 
     #[test]
     fn test_part_1() {
-        assert!(run(INPUT, false) == 0);
-        let results = run(include_str!("../input/day_21.txt"), false);
-        println!("{}", results);
-        assert!(results == 0);
+        assert_eq!(run(INPUT, false), 5);
+        assert_eq!(run(include_str!("../input/day_21.txt"), false), 2779);
     }
 
     #[test]
     fn test_part_2() {
-        assert!(run(INPUT, true) == 0);
-        let results = run(include_str!("../input/day_21.txt"), true);
-        println!("{}", results);
-        assert!(results == 0);
+        let (allergens, _) = parse_input(INPUT);
+        assert_eq!(
+            find_danderous_ingrediants(allergens).join(","),
+            "mxmxvkd,sqjhc,fvjkl"
+        );
+        let (allergens_2, _) = parse_input(include_str!("../input/day_21.txt"));
+        assert_eq!(
+            find_danderous_ingrediants(allergens_2).join(","),
+            "lkv,lfcppl,jhsrjlj,jrhvk,zkls,qjltjd,xslr,rfpbpn"
+        );
     }
 }
--- 2020/src/day_22.rs
+++ 2020/src/day_22.rs
@@ -1,29 +1,106 @@
-pub fn run(_input: &str, part_two: bool) -> i64 {
+use itertools::Itertools;
+use std::collections::{HashSet, VecDeque};
+
+fn find_score(deck: VecDeque<i64>) -> i64 {
+    deck.iter().enumerate().fold(0, |total, (pos, card)| {
+        total + (*card * (deck.len() - pos) as i64)
+    })
+}
+
+fn recursive_combat(
+    mut player_1: VecDeque<i64>,
+    mut player_2: VecDeque<i64>,
+    score: bool,
+) -> (usize, i64) {
+    let mut previous_round = HashSet::new();
+    loop {
+        if !previous_round.insert((player_1.clone(), player_2.clone())) || player_2.is_empty() {
+            break (1, if score { find_score(player_1) } else { 0 });
+        } else if player_1.is_empty() {
+            break (2, find_score(player_2));
+        }
+
+        let (c_1, c_2) = (player_1.pop_front().unwrap(), player_2.pop_front().unwrap());
+        if c_1 <= player_1.len() as i64 && c_2 <= player_2.len() as i64 {
+            match recursive_combat(
+                player_1.iter().take(c_1 as usize).cloned().collect(),
+                player_2.iter().take(c_2 as usize).cloned().collect(),
+                false,
+            ) {
+                (1, _) => player_1.extend([c_1, c_2].iter()),
+                (2, _) => player_2.extend([c_2, c_1].iter()),
+                _ => unreachable!(),
+            }
+        } else if c_1 > c_2 {
+            player_1.extend([c_1, c_2].iter());
+        } else {
+            player_2.extend([c_2, c_1].iter());
+        }
+    }
+}
+
+fn find_winner(mut player_1: VecDeque<i64>, mut player_2: VecDeque<i64>) -> i64 {
+    loop {
+        if player_1.is_empty() {
+            break find_score(player_2);
+        } else if player_2.is_empty() {
+            break find_score(player_1);
+        }
+        let (c_1, c_2) = (player_1.pop_front().unwrap(), player_2.pop_front().unwrap());
+        if c_1 > c_2 {
+            player_1.extend([c_1, c_2].iter());
+        } else {
+            player_2.extend([c_2, c_1].iter());
+        }
+    }
+}
+
+pub fn run(input: &str, part_two: bool) -> i64 {
+    let (player_1, player_2): (VecDeque<i64>, VecDeque<i64>) = input
+        .split("\n\n")
+        .map(|lines| {
+            lines
+                .lines()
+                .skip(1)
+                .map(|line| line.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect_tuple()
+        .unwrap();
     if part_two {
-        0
+        let (_, score) = recursive_combat(player_1, player_2, true);
+        score
     } else {
-        0
+        find_winner(player_1, player_2)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10";
 
     #[test]
     fn test_part_1() {
-        assert!(run(INPUT, false) == 0);
-        let results = run(include_str!("../input/day_22.txt"), false);
-        println!("{}", results);
-        assert!(results == 0);
+        assert_eq!(run(INPUT, false), 306);
+        assert_eq!(run(include_str!("../input/day_22.txt"), false), 32366);
     }
 
     #[test]
     fn test_part_2() {
-        assert!(run(INPUT, true) == 0);
-        let results = run(include_str!("../input/day_22.txt"), true);
-        println!("{}", results);
-        assert!(results == 0);
+        assert_eq!(run(INPUT, true), 291);
+        //assert_eq!(run(include_str!("../input/day_22.txt"), true), 30891);
     }
 }
