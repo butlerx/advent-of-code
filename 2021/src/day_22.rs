743c813e402f8f6842b94288a00e52870c28ac38
butlerx <butlerx@notthe.cloud>
2021-11-30 15:10:41 +0000

    add 2021 scaffold

---
+++ 2021/src/day_22.rs
@@ -0,0 +1,14 @@
+pub fn run(input: &str, part_two: bool) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {}
+
+    #[test]
+    fn test_part_2() {}
+}
