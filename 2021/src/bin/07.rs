246158eed5e670762476aba948a40e10333977b5
butlerx <butlerx@notthe.cloud>
2021-12-07 08:44:39 +0000

    day 7 2021

--- 2021/src/bin/07.rs
+++ 2021/src/bin/07.rs
@@ -1,33 +1,57 @@
+use itertools::Itertools;
+
+static COMMA: &str = ",";
+
 fn main() {
     let input = include_str!("../../input/07.txt");
-    println!("Part 1: {}", part_1(input));
-    println!("Part 2: {}", part_2(input));
+    println!("Part 1: {}", run(input, false));
+    println!("Part 2: {}", run(input, true));
 }
 
-
-pub fn part_1(input: &str) -> i64 {
-    0
+fn run(input: &str, exponential: bool) -> usize {
+    let mut nums: Vec<i64> = input
+        .split(COMMA)
+        .map(|n| n.trim().parse::<i64>().unwrap())
+        .collect();
+    nums.sort_unstable();
+    let counts: Vec<(i64, usize)> = nums
+        .iter()
+        .dedup_with_count()
+        .map(|(a, b)| (*b, a))
+        .collect();
+    (counts.first().unwrap().0..=counts.last().unwrap().0)
+        .map(|candidate| {
+            counts
+                .iter()
+                .map(|(pos, count)| cost((pos - candidate).abs() as usize, exponential) * count)
+                .sum()
+        })
+        .min()
+        .unwrap()
 }
 
-pub fn part_2(input: &str) -> i64 {
-    0
+fn cost(num: usize, exponential: bool) -> usize {
+    if exponential {
+        (num * (num + 1)) / 2
+    } else {
+        num
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(include_str!("../../input/07.txt")), 0);
+        assert_eq!(run(INPUT, false), 37);
+        assert_eq!(run(include_str!("../../input/07.txt"), false), 325528);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(include_str!("../../input/07.txt")), 0);
+        assert_eq!(run(INPUT, true), 168);
+        assert_eq!(run(include_str!("../../input/07.txt"), true), 85015836);
     }
 }
-
