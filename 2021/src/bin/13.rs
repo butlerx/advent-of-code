d4d2282207d80cc3d5df33ed745ab4c91cc094d0
butlerx <butlerx@notthe.cloud>
2021-12-13 09:55:57 +0000

    day 13 2021

--- 2021/src/bin/13.rs
+++ 2021/src/bin/13.rs
@@ -1,31 +1,118 @@
+use itertools::Itertools;
+use std::iter::FromIterator;
+
+type Dots = Vec<(usize, usize)>;
+type Instructions = Vec<(char, usize)>;
+
 fn main() {
     let input = include_str!("../../input/13.txt");
     println!("Part 1: {}", part_1(input));
-    println!("Part 2: {}", part_2(input));
+    println!("Part 2: \n{}", part_2(input));
 }
 
-pub fn part_1(_input: &str) -> i64 {
-    0
+fn parse_input(input: &str) -> (Dots, Instructions) {
+    let (positions, instructions) = input.split_once("\n\n").unwrap();
+    (
+        positions
+            .lines()
+            .map(|lines| {
+                let (x, y) = lines.split_once(',').unwrap();
+                (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
+            })
+            .collect(),
+        instructions
+            .lines()
+            .map(|line| {
+                let args = line.replace("fold along", "");
+                let (l, n) = args.trim().split_once('=').unwrap();
+                (l.parse::<char>().unwrap(), n.parse::<usize>().unwrap())
+            })
+            .collect(),
+    )
 }
 
-pub fn part_2(_input: &str) -> i64 {
-    0
+fn fold(dots: Dots, folds: Instructions) -> Dots {
+    dots.into_iter()
+        .map(|pos| {
+            folds.iter().fold(pos, |(x, y), &(l, n)| match l {
+                'x' if x > n => ((2 * n) - x, y),
+                'y' if y > n => (x, (2 * n) - y),
+                _ => (x, y),
+            })
+        })
+        .unique()
+        .collect()
+}
+
+fn part_1(input: &str) -> usize {
+    let (dots, mut folds) = parse_input(input);
+    folds.truncate(1);
+    fold(dots, folds).len()
+}
+
+fn part_2(input: &str) -> String {
+    let (dots, folds) = parse_input(input);
+    let folded_dots = fold(dots, folds);
+    let (max_x, max_y) = folded_dots.iter().fold((0, 0), |(max_x, max_y), &(x, y)| {
+        (max_x.max(x), max_y.max(y))
+    });
+    folded_dots
+        .iter()
+        .fold(
+            vec![vec![' '; max_x + 1]; max_y + 1],
+            |mut matrix, &(x, y)| {
+                matrix[y][x] = '#';
+                matrix
+            },
+        )
+        .into_iter()
+        .map(String::from_iter)
+        .join("\n")
 }
 
 #[cfg(test)]
 mod day_13_tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(include_str!("../../input/13.txt")), 0);
+        assert_eq!(part_1(INPUT), 17);
+        assert_eq!(part_1(include_str!("../../input/13.txt")), 790);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(include_str!("../../input/13.txt")), 0);
+        assert_eq!(part_2(INPUT), "#####\n#   #\n#   #\n#   #\n#####");
+        assert_eq!(
+            "\n".to_owned() + &part_2(include_str!("../../input/13.txt")),
+            "
+###   ##  #  # #### ###  ####   ##  ## 
+#  # #  # #  #    # #  # #       # #  #
+#  # #    ####   #  ###  ###     # #   
+###  # ## #  #  #   #  # #       # #   
+#    #  # #  # #    #  # #    #  # #  #
+#     ### #  # #### ###  #     ##   ## "
+        );
     }
 }
