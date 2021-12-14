5fd73c0a67691fb901b069047da266ad54c728f8
butlerx <butlerx@notthe.cloud>
2021-12-14 09:46:55 +0000

    day 14 2021

--- 2021/src/bin/14.rs
+++ 2021/src/bin/14.rs
@@ -1,31 +1,91 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("../../input/14.txt");
-    println!("Part 1: {}", part_1(input));
-    println!("Part 2: {}", part_2(input));
+    println!("Part 1: {}", run(input, 10));
+    println!("Part 2: {}", run(input, 40));
 }
 
-pub fn part_1(_input: &str) -> i64 {
-    0
+type PairCount = HashMap<(char, char), i64>;
+type CharCount = HashMap<char, i64>;
+type Rules = HashMap<(char, char), char>;
+
+fn parse_input(input: &str) -> ((PairCount, CharCount), Rules) {
+    let (template, rules) = input.split_once("\n\n").unwrap();
+
+    let pair_count = template
+        .chars()
+        .tuple_windows()
+        .fold(HashMap::new(), |mut counts, window| {
+            *counts.entry(window).or_default() += 1;
+            counts
+        });
+    let char_count = template.chars().fold(HashMap::new(), |mut counts, c| {
+        *counts.entry(c).or_default() += 1;
+        counts
+    });
+    let pairs = rules
+        .lines()
+        .map(|line| {
+            let (l, n) = line.split_once(" -> ").unwrap();
+            let mut letters = l.chars().tuple_windows();
+            (letters.next().unwrap(), n.parse::<char>().unwrap())
+        })
+        .collect();
+    ((pair_count, char_count), pairs)
 }
 
-pub fn part_2(_input: &str) -> i64 {
-    0
+fn run(input: &str, steps: usize) -> i64 {
+    let (counts, rules) = parse_input(input);
+    let (_, results) = (0..steps).fold(counts, |(pair_count, mut char_count), _| {
+        let mut new_pair_count = pair_count.clone();
+
+        for ((left, right), count) in pair_count {
+            let middle = *rules.get(&(left, right)).unwrap();
+            *new_pair_count.entry((left, right)).or_default() -= count;
+            *new_pair_count.entry((left, middle)).or_default() += count;
+            *new_pair_count.entry((middle, right)).or_default() += count;
+            *char_count.entry(middle).or_default() += count;
+        }
+
+        (new_pair_count, char_count)
+    });
+
+    results.values().max().unwrap() - results.values().min().unwrap()
 }
 
 #[cfg(test)]
 mod day_14_tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(include_str!("../../input/14.txt")), 0);
+        assert_eq!(run(INPUT, 10), 1588);
+        assert_eq!(run(include_str!("../../input/14.txt"), 10), 2068);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(include_str!("../../input/14.txt")), 0);
+        assert_eq!(run(INPUT, 40), 2188189693529);
+        assert_eq!(run(include_str!("../../input/14.txt"), 40), 2158894777814);
     }
 }
