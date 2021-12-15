c802a82ea658134d61a0d81aeb390ada2bb51383
butlerx <butlerx@notthe.cloud>
2021-12-15 10:05:12 +0000

    day 15 2021

--- 2021/src/bin/15.rs
+++ 2021/src/bin/15.rs
@@ -1,31 +1,90 @@
+use std::collections::BinaryHeap;
+
 fn main() {
     let input = include_str!("../../input/15.txt");
     println!("Part 1: {}", part_1(input));
     println!("Part 2: {}", part_2(input));
 }
 
-fn part_1(_input: &str) -> i64 {
-    0
+fn shortest_path(map: Vec<Vec<i64>>) -> i64 {
+    let success = (map.len() - 1, map[0].len() - 1);
+    let mut costs = vec![vec![i64::MAX; map[0].len()]; map.len()];
+    let mut to_see = BinaryHeap::new();
+    to_see.push((0, 0, 0));
+    loop {
+        let (cost, x, y) = to_see.pop().unwrap();
+        if (x, y) == success {
+            break -cost;
+        }
+        if -cost < costs[x][y] {
+            let neighbours = vec![
+                (x.saturating_sub(1), y),
+                (x + 1, y),
+                (x, y + 1),
+                (x, y.saturating_sub(1)),
+            ];
+            for (x1, y1) in neighbours {
+                if map.get(x1).and_then(|row| row.get(y1)).is_none() {
+                    continue;
+                }
+                let next_cost = -cost + map[x1][y1];
+                if next_cost < costs[x1][y1] {
+                    to_see.push((-next_cost, x1, y1));
+                    costs[x1][y1] = next_cost;
+                }
+            }
+        }
+    }
 }
 
-fn part_2(_input: &str) -> i64 {
-    0
+fn parse_input(input: &str) -> Vec<Vec<i64>> {
+    input
+        .lines()
+        .map(|l| l.bytes().map(|c| (c - b'0') as i64).collect())
+        .collect()
+}
+
+fn part_1(input: &str) -> i64 {
+    let map = parse_input(input);
+    shortest_path(map)
+}
+
+fn part_2(input: &str) -> i64 {
+    let map = parse_input(input);
+    let (nr, nc) = (map.len(), map[0].len());
+    let expanded_map = (0..(5 * map.len()))
+        .map(|r| {
+            (0..(5 * map[0].len()))
+                .map(|c| (map[r % nr][c % nc] + (r / nr + c / nc) as i64 - 1) % 9 + 1)
+                .collect()
+        })
+        .collect();
+    shortest_path(expanded_map)
 }
 
 #[cfg(test)]
 mod day_15_tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(include_str!("../../input/15.txt")), 0);
+        assert_eq!(part_1(INPUT), 40);
+        assert_eq!(part_1(include_str!("../../input/15.txt")), 583);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(include_str!("../../input/15.txt")), 0);
+        assert_eq!(part_2(INPUT), 315);
+        assert_eq!(part_2(include_str!("../../input/15.txt")), 2927);
     }
 }
