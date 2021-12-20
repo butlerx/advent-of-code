7c87408bee2d8d7d6272ac504cfa6ec8ac46479d
butlerx <butlerx@notthe.cloud>
2021-12-20 14:39:37 +0000

    clean up day 20

--- 2021/src/bin/20.rs
+++ 2021/src/bin/20.rs
@@ -17,39 +17,21 @@ fn part_2(input: &str) -> usize {
 fn parse_input(input: &str, iterations: usize) -> (ImageMap, Vec<bool>) {
     let (r, img) = input.split_once("\n\n").unwrap();
     let first_line = r.replace("\n", "");
-    let rules = first_line
-        .chars()
-        .map(|c| match c {
-            '.' => false,
-            '#' => true,
-            _ => unreachable!(),
-        })
-        .collect();
+    let rules = first_line.chars().map(|c| c == '#').collect();
 
-    let mut size = None;
     let map: ImageMap = img
         .lines()
-        .enumerate()
-        .map(|(li, line)| {
-            let mut map_line = vec![false; line.len() + iterations * 2];
-            for (i, c) in line.chars().enumerate() {
-                match c {
-                    '.' => continue,
-                    '#' => *map_line.get_mut(i + iterations).unwrap() = true,
-                    _ => unreachable!(),
-                }
-            }
-
-            if li == 0 {
-                size = Some(map_line.len());
-            }
+        .map(|line| {
+            let mut map_line = vec![false; iterations]
+                .into_iter()
+                .chain(line.chars().map(|c| c == '#'))
+                .collect::<Vec<bool>>();
+            map_line.resize(line.len() + iterations * 2, false);
             map_line
         })
         .collect();
 
-    let padding: ImageMap = (0..iterations)
-        .map(|_| vec![false; size.unwrap()])
-        .collect();
+    let padding: ImageMap = vec![vec![false; map[0].len() + iterations * 2]; iterations];
     let mut res_map = padding.clone();
     res_map.extend(map);
     res_map.extend(padding);
@@ -59,7 +41,6 @@ fn parse_input(input: &str, iterations: usize) -> (ImageMap, Vec<bool>) {
 
 fn process_image(input: &str, iterations: usize) -> usize {
     let (image_map, rules) = parse_input(input, iterations);
-
     let (map, _) = (0..iterations).fold((image_map, false), |(map, default), _| {
         (
             map.iter()
@@ -86,15 +67,18 @@ fn process_image(input: &str, iterations: usize) -> usize {
 
 fn get_new_pixel(rc: (usize, usize), map: &[Vec<bool>], rules: &[bool], default: bool) -> bool {
     let (r, c) = (rc.0 as isize, rc.1 as isize);
-    rules[(r - 1..r + 2).fold(0, |num, row| {
-        (c - 1..c + 2).fold(num, |n, col| {
-            let number = n << 1;
+    let pixel = |col: isize, row: isize| {
+        if col < 0 || row < 0 || col as usize >= map.len() || row as usize >= map.len() {
+            default
+        } else {
+            map[row as usize][col as usize]
+        }
+    };
 
-            if if col < 0 || row < 0 || col as usize >= map.len() || r as usize >= map.len() {
-                default
-            } else {
-                map[r as usize][c as usize]
-            } {
+    rules[(r - 1..r + 2).fold(0, |n, row| {
+        (c - 1..c + 2).fold(n, |num, col| {
+            let number = num << 1;
+            if pixel(col, row) {
                 number + 1
             } else {
                 number
