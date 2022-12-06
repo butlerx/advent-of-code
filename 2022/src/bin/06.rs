d3d6f876f3c1ea3c698ff29f3e938f4400e5326b
Cian Butler <butlerx@notthe.cloud>
2022-12-06 06:17:45 +0000

    2022 day 6 soltuion

--- 2022/src/bin/06.rs
+++ 2022/src/bin/06.rs
@@ -1,32 +1,56 @@
-static INPUT_TXT: &str = include_str!("../../input/06.txt");
+use std::collections::HashSet;
+
+static INPUT_TXT: &[u8] = include_bytes!("../../input/06.txt");
 
 fn main() {
     println!("Part 1: {}", part_1(INPUT_TXT));
     println!("Part 2: {}", part_2(INPUT_TXT));
 }
 
-fn part_1(_input: &str) -> i64 {
-    0
+fn marker_pos(input: &[u8], window_size: usize) -> usize {
+    input
+        .windows(window_size)
+        .enumerate()
+        .find(|(_, s)| s.into_iter().collect::<HashSet<_>>().len() == s.len())
+        .unwrap()
+        .0
+        + window_size
 }
 
-fn part_2(_input: &str) -> i64 {
-    0
+fn part_1(input: &[u8]) -> usize {
+    marker_pos(input, 4)
+}
+
+fn part_2(input: &[u8]) -> usize {
+    marker_pos(input, 14)
 }
 
 #[cfg(test)]
 mod day_6_tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT_1: &[u8] = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes();
+    static INPUT_2: &[u8] = "bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes();
+    static INPUT_3: &[u8] = "nppdvjthqldpwncqszvftbrmjlhg".as_bytes();
+    static INPUT_4: &[u8] = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes();
+    static INPUT_5: &[u8] = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes();
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(INPUT_TXT), 0);
+        assert_eq!(part_1(INPUT_1), 7);
+        assert_eq!(part_1(INPUT_2), 5);
+        assert_eq!(part_1(INPUT_3), 6);
+        assert_eq!(part_1(INPUT_4), 10);
+        assert_eq!(part_1(INPUT_5), 11);
+        assert_eq!(part_1(INPUT_TXT), 1155);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(INPUT_TXT), 0);
+        assert_eq!(part_2(INPUT_1), 19);
+        assert_eq!(part_2(INPUT_2), 23);
+        assert_eq!(part_2(INPUT_3), 23);
+        assert_eq!(part_2(INPUT_4), 29);
+        assert_eq!(part_2(INPUT_5), 26);
+        assert_eq!(part_2(INPUT_TXT), 2789);
     }
 }
