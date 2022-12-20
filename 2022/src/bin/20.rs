a7b7a94832b4008ee54e7d70e51f7b073459c6cb
Cian Butler <butlerx@notthe.cloud>
2022-12-20 07:31:55 +0000

    2022 day 20 solution

--- 2022/src/bin/20.rs
+++ 2022/src/bin/20.rs
@@ -5,28 +5,65 @@ fn main() {
     println!("Part 2: {}", part_2(INPUT_TXT));
 }
 
-fn part_1(_input: &str) -> i64 {
-    0
+fn mix(nums: &[i64], iterations: usize) -> Vec<usize> {
+    (0..iterations).fold((0..nums.len()).collect(), |res, _| {
+        nums.iter().enumerate().fold(res, |mut res, (i, &x)| {
+            let pos = res.iter().position(|&y| y == i).unwrap();
+            res.remove(pos);
+            res.insert((pos as i64 + x).rem_euclid(res.len() as i64) as usize, i);
+            res
+        })
+    })
 }
 
-fn part_2(_input: &str) -> i64 {
-    0
+fn decrypt(nums: Vec<i64>, iterations: usize) -> i64 {
+    let orig_zero_pos = nums.iter().position(|&i| i == 0).unwrap();
+    let ans = mix(&nums, iterations);
+    let zero_pos = ans.iter().position(|&i| i == orig_zero_pos).unwrap();
+    [1000, 2000, 3000]
+        .iter()
+        .map(|i| nums[ans[(zero_pos + i) % ans.len()]])
+        .sum()
+}
+
+fn part_1(input: &str) -> i64 {
+    let nums = input
+        .trim()
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect::<Vec<_>>();
+    decrypt(nums, 1)
+}
+
+fn part_2(input: &str) -> i64 {
+    let nums = input
+        .trim()
+        .lines()
+        .map(|l| l.parse::<i64>().unwrap() * 811_589_153)
+        .collect::<Vec<_>>();
+    decrypt(nums, 10)
 }
 
 #[cfg(test)]
 mod day_20_tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "1
+2
+-3
+3
+-2
+0
+4";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(INPUT_TXT), 0);
+        assert_eq!(part_1(INPUT), 3);
+        assert_eq!(part_1(INPUT_TXT), 4267);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(INPUT_TXT), 0);
+        assert_eq!(part_2(INPUT), 1_623_178_306);
+        assert_eq!(part_2(INPUT_TXT), 6_871_725_358_451);
     }
 }
