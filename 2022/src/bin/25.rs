b1cec785dfd0a3e2cfa5cf3b1dbd2d326a83db75
Cian Butler <butlerx@notthe.cloud>
2022-12-25 08:29:46 +0000

    2022 day 25 solution

--- 2022/src/bin/25.rs
+++ 2022/src/bin/25.rs
@@ -2,31 +2,76 @@ static INPUT_TXT: &str = include_str!("../../input/25.txt");
 
 fn main() {
     println!("Part 1: {}", part_1(INPUT_TXT));
-    println!("Part 2: {}", part_2(INPUT_TXT));
 }
 
-fn part_1(_input: &str) -> i64 {
-    0
+fn dec_to_snafu(mut num: i64) -> String {
+    let mut res = Vec::new();
+    while num > 0 {
+        let c = match (num % 5) as u32 {
+            d if d < 3 => std::char::from_digit(d, 10).unwrap(),
+            d if d == 3 => {
+                num += 2;
+                '='
+            }
+            _ => {
+                num += 1;
+                '-'
+            }
+        };
+        res.push(c);
+        num /= 5;
+    }
+    res.reverse();
+    res.into_iter().collect()
 }
 
-fn part_2(_input: &str) -> i64 {
-    0
+fn snafu_to_dec_sum(input: &str) -> i64 {
+    input
+        .trim()
+        .lines()
+        .map(|l| {
+            l.chars().fold(0, |i, n| {
+                let num = match n {
+                    '-' => -1,
+                    '=' => -2,
+                    _ => n.to_digit(10).unwrap() as i64,
+                };
+                num + (i * 5)
+            })
+        })
+        .sum()
+}
+
+fn part_1(input: &str) -> String {
+    dec_to_snafu(snafu_to_dec_sum(input))
 }
 
 #[cfg(test)]
 mod day_25_tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122";
 
     #[test]
-    fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(INPUT_TXT), 0);
+    fn test_snafu_to_dec_sum() {
+        assert_eq!(snafu_to_dec_sum(INPUT), 4890);
+        assert_eq!(snafu_to_dec_sum(INPUT_TXT), 29600609295066);
     }
 
     #[test]
-    fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(INPUT_TXT), 0);
+    fn test_part_1() {
+        assert_eq!(part_1(INPUT), "2=-1=0".to_string());
+        assert_eq!(part_1(INPUT_TXT), "2=--00--0220-0-21==1".to_string());
     }
 }
