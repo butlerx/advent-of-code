505a1722337b4b09527a9e1f34f94dbddabf8fd3
Cian Butler <butlerx@notthe.cloud>
2023-12-06 11:32:07 +0000

    Decided to see how much quicker itd be not bruteforced time went from 0.78s to 0.00s

--- 2023/src/bin/06.rs
+++ 2023/src/bin/06.rs
@@ -5,21 +5,31 @@ fn main() {
     println!("Part 2: {}", part_2(INPUT_TXT));
 }
 
-fn possible_wins(time: usize, dist: usize) -> usize {
+//  Original Solution bruteforced it
+#[allow(dead_code)]
+fn possible_wins_brute_force(time: usize, dist: usize) -> usize {
     (1..time)
         .map(|held| held * (time - held))
         .filter(|possible_dist| possible_dist > &dist)
         .count()
 }
 
-fn part_1(input: &str) -> usize {
+// Maths solution
+fn possible_wins(time: f64, dist: f64) -> u64 {
+    let d = (time * time - 4.0 * (dist + 1.0)).sqrt();
+    let low = ((time - d) / 2.0).max(0.0).ceil() as u64;
+    let high = ((time + d) / 2.0).floor() as u64;
+    (high - low) + 1
+}
+
+fn part_1(input: &str) -> u64 {
     let nums: Vec<_> = input
         .trim()
         .split('\n')
         .map(|line| {
             line.split_whitespace()
                 .skip(1)
-                .map(|x| x.parse::<usize>().unwrap())
+                .map(|x| x.parse::<f64>().unwrap())
                 .collect::<Vec<_>>()
         })
         .collect();
@@ -30,7 +40,7 @@ fn part_1(input: &str) -> usize {
         .product()
 }
 
-fn part_2(input: &str) -> usize {
+fn part_2(input: &str) -> u64 {
     let nums: Vec<_> = input
         .trim()
         .split('\n')
@@ -38,7 +48,7 @@ fn part_2(input: &str) -> usize {
             line.split_whitespace()
                 .skip(1)
                 .collect::<String>()
-                .parse::<usize>()
+                .parse::<f64>()
                 .unwrap()
         })
         .collect();
