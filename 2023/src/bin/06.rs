b94f14bbc0ce20721bf11d12a291eb7343380bd8
Cian Butler <butlerx@notthe.cloud>
2023-12-06 06:42:01 +0000

    2023: day 6 solution

--- 2023/src/bin/06.rs
+++ 2023/src/bin/06.rs
@@ -5,28 +5,61 @@ fn main() {
     println!("Part 2: {}", part_2(INPUT_TXT));
 }
 
-fn part_1(_input: &str) -> u32 {
-    0
+fn possible_wins(time: usize, dist: usize) -> usize {
+    (1..time)
+        .map(|held| held * (time - held))
+        .filter(|possible_dist| possible_dist > &dist)
+        .count()
 }
 
-fn part_2(_input: &str) -> u32 {
-    0
+fn part_1(input: &str) -> usize {
+    let nums: Vec<_> = input
+        .trim()
+        .split('\n')
+        .map(|line| {
+            line.split_whitespace()
+                .skip(1)
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    nums[0]
+        .iter()
+        .zip(nums[1].iter())
+        .map(|(time, dist)| possible_wins(*time, *dist))
+        .product()
+}
+
+fn part_2(input: &str) -> usize {
+    let nums: Vec<_> = input
+        .trim()
+        .split('\n')
+        .map(|line| {
+            line.split_whitespace()
+                .skip(1)
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap()
+        })
+        .collect();
+    possible_wins(nums[0], nums[1])
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    static INPUT: &str = "";
+    static INPUT: &str = "Time:      7  15   30
+Distance:  9  40  200";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(INPUT_TXT), 0);
+        assert_eq!(part_1(INPUT), 288);
+        assert_eq!(part_1(INPUT_TXT), 781_200);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT), 0);
-        assert_eq!(part_2(INPUT_TXT), 0);
+        assert_eq!(part_2(INPUT), 71503);
+        assert_eq!(part_2(INPUT_TXT), 49_240_091);
     }
 }
