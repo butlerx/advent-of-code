19274f300459e634741ea0952978f60fb6bfbf70
Cian Butler <butlerx@notthe.cloud>
2024-12-01 06:47:19 +0000

    Day 1 2024 solution

--- 2024/src/bin/01.rs
+++ 2024/src/bin/01.rs
@@ -5,29 +5,65 @@ fn main() {
     println!("Part 2: {}", part_2(INPUT_TXT));
 }
 
-fn part_1(_input: &str) -> u32 {
-    0
+fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let (mut col_a, mut col_b): (Vec<_>, Vec<_>) = input
+        .lines()
+        .map(|line| {
+            let (a, b) = line
+                .split_once(char::is_whitespace)
+                .expect("invalid line format");
+            (
+                a.trim().parse::<u32>().expect("not a valid number"),
+                b.trim().parse::<u32>().expect("not a valid number"),
+            )
+        })
+        .unzip();
+
+    col_a.sort();
+    col_b.sort();
+
+    (col_a, col_b)
 }
 
-fn part_2(_input: &str) -> u32 {
-    0
+fn part_1(input: &str) -> u32 {
+    let (col_a, col_b) = parse_input(input);
+    col_a
+        .iter()
+        .zip(col_b.iter())
+        .map(|(a, b)| if a > b { a - b } else { b - a })
+        .sum()
+}
+
+fn part_2(input: &str) -> u32 {
+    let (col_a, col_b) = parse_input(input);
+    col_a
+        .iter()
+        .map(|a| {
+            let c = col_b.iter().filter(|&b| b == a).count() as u32;
+            a * c
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    static INPUT: &str = "";
-    static INPUT_2: &str = "";
+    static INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(INPUT_TXT), 0);
+        assert_eq!(part_1(INPUT), 11);
+        assert_eq!(part_1(INPUT_TXT), 2000468);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT_2), 0);
-        assert_eq!(part_2(INPUT_TXT), 0);
+        assert_eq!(part_2(INPUT), 31);
+        assert_eq!(part_2(INPUT_TXT), 18567089);
     }
 }
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::pedantic)]
+
 fn main() {
     println!("Hello, world!");
 }
