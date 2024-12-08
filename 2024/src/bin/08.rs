1ab56c752159e8fef6d63bca5d44bd7474cfa542
Cian Butler <butlerx@notthe.cloud>
2024-12-08 10:40:21 +0000

    Clean up code a little

--- 2024/src/bin/08.rs
+++ 2024/src/bin/08.rs
@@ -33,14 +33,19 @@ impl Point {
         }
     }
 
-    fn in_bounds(&self, bounds: &Self) -> bool {
-        (0..=bounds.x).contains(&self.x) && (0..=bounds.y).contains(&self.y)
+    fn in_bounds(&self, x: i32, y: i32) -> bool {
+        (0..=x).contains(&self.x) && (0..=y).contains(&self.y)
     }
 
-    fn generate_sequence(self, delta: Self, bounds: &Self) -> impl Iterator<Item = Self> + use<'_> {
+    fn generate_sequence(
+        self,
+        delta: Self,
+        x: i32,
+        y: i32,
+    ) -> impl Iterator<Item = Self> + 'static {
         std::iter::successors(Some(self), move |&point| {
             let next = point + delta;
-            next.in_bounds(bounds).then_some(next)
+            next.in_bounds(x, y).then_some(next)
         })
     }
 }
@@ -78,63 +83,74 @@ impl Neg for Point {
     }
 }
 
-type Map = HashMap<char, Vec<Point>>;
-
-fn parse_input(input: &str) -> (Map, Point) {
-    let parsed = input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| {
-            line.chars()
-                .enumerate()
-                .filter(|(_, c)| c.is_ascii_alphanumeric())
-                .map(move |(x, c)| (c, Point::new(x, y)))
-        })
-        .fold(HashMap::new(), |mut map: Map, (c, point)| {
-            map.entry(c).or_default().push(point);
-            map
-        });
-
-    let max_boundary = Point::new(
-        input.lines().next().map_or(0, |line| line.len() - 1),
-        input.lines().count() - 1,
-    );
-
-    (parsed, max_boundary)
+struct Map {
+    antenna: HashMap<char, Vec<Point>>,
+    max_x: i32,
+    max_y: i32,
 }
 
-fn find_antenna(antenna: &Map, bounds: &Point, include_path: bool) -> usize {
-    antenna
-        .values()
-        .flat_map(|beacon_list| {
-            beacon_list.iter().enumerate().flat_map(|(i, first)| {
-                beacon_list[i + 1..].iter().flat_map(move |second| {
-                    let delta = *second - *first;
-                    if include_path {
-                        let negative = first.generate_sequence(-delta, bounds);
-                        let positive = first.generate_sequence(delta, bounds);
-                        negative.chain(positive.skip(1)).collect::<HashSet<_>>()
-                    } else {
-                        [*first - delta, *second + delta]
-                            .into_iter()
-                            .filter(|point| point.in_bounds(bounds))
-                            .collect()
-                    }
+impl From<&str> for Map {
+    fn from(input: &str) -> Self {
+        let antenna = input
+            .lines()
+            .enumerate()
+            .flat_map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .filter(|(_, c)| c.is_ascii_alphanumeric())
+                    .map(move |(x, c)| (c, Point::new(x, y)))
+            })
+            .fold(
+                HashMap::new(),
+                |mut map: HashMap<char, Vec<Point>>, (c, point)| {
+                    map.entry(c).or_default().push(point);
+                    map
+                },
+            );
+        let max_boundary = Point::new(
+            input.lines().next().map_or(0, |line| line.len() - 1),
+            input.lines().count() - 1,
+        );
+        Self {
+            antenna,
+            max_x: max_boundary.x,
+            max_y: max_boundary.y,
+        }
+    }
+}
+
+impl Map {
+    fn find_antenna(&self, include_path: bool) -> usize {
+        self.antenna
+            .values()
+            .flat_map(|beacon_list| {
+                beacon_list.iter().enumerate().flat_map(|(i, first)| {
+                    beacon_list[i + 1..].iter().flat_map(move |second| {
+                        let delta = *second - *first;
+                        if include_path {
+                            let negative = first.generate_sequence(-delta, self.max_x, self.max_y);
+                            let positive = first.generate_sequence(delta, self.max_x, self.max_y);
+                            negative.chain(positive.skip(1)).collect::<HashSet<_>>()
+                        } else {
+                            [*first - delta, *second + delta]
+                                .into_iter()
+                                .filter(|point| point.in_bounds(self.max_x, self.max_y))
+                                .collect()
+                        }
+                    })
                 })
             })
-        })
-        .collect::<HashSet<_>>()
-        .len()
+            .collect::<HashSet<_>>()
+            .len()
+    }
 }
 
 fn part_1(input: &str) -> usize {
-    let (antenna, bounds) = parse_input(input);
-    find_antenna(&antenna, &bounds, false)
+    Map::from(input).find_antenna(false)
 }
 
 fn part_2(input: &str) -> usize {
-    let (antenna, bounds) = parse_input(input);
-    find_antenna(&antenna, &bounds, true)
+    Map::from(input).find_antenna(true)
 }
 
 #[cfg(test)]
