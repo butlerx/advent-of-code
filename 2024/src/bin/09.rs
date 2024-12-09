b21fc299617e3d7e607022808cd16e1c01837e51
Cian Butler <butlerx@notthe.cloud>
2024-12-09 07:46:49 +0000

    2024 day 9 solutions

--- 2024/src/bin/09.rs
+++ 2024/src/bin/09.rs
@@ -1,33 +1,100 @@
+use std::time::Instant;
+
 static INPUT_TXT: &str = include_str!("../../input/09.txt");
 
 fn main() {
-    println!("Part 1: {}", part_1(INPUT_TXT));
-    println!("Part 2: {}", part_2(INPUT_TXT));
+    println!("🌟 --- Day 9 Results --- 🌟");
+    let start_1 = Instant::now();
+    let res_1 = part_1(INPUT_TXT);
+    let duration_1 = start_1.elapsed().as_millis();
+    println!("📌 Part 1: {res_1}, complete in {duration_1} ms");
+
+    let start_2 = Instant::now();
+    let res_2 = part_2(INPUT_TXT);
+    let duration_2 = start_2.elapsed().as_millis();
+    println!("📌 Part 2: {res_2}, complete in {duration_2} ms");
 }
 
-fn part_1(_input: &str) -> u32 {
-    0
+fn compact_disk(disk: Vec<(usize, i32)>) -> usize {
+    (0..disk.len())
+        .rev()
+        .fold(disk, |mut acc, i| {
+            let (size, id) = acc[i];
+            if id == -1 {
+                return acc;
+            }
+            if let Some(j) = acc[0..i].iter().position(|&(s, id)| id == -1 && size <= s) {
+                let (empty_size, _) = acc[j];
+                acc[j] = (size, id);
+                acc[i] = (size, -1);
+                if size < empty_size {
+                    acc.insert(j + 1, (empty_size - size, -1));
+                }
+            }
+            acc
+        })
+        .iter()
+        .flat_map(|&(s, id)| std::iter::repeat(id).take(s))
+        .enumerate()
+        .filter(|&(_, id)| id != -1)
+        .map(|(i, id)| i * id as usize)
+        .sum()
 }
 
-fn part_2(_input: &str) -> u32 {
-    0
+fn part_1(input: &str) -> usize {
+    let mut id = 0;
+    compact_disk(
+        input
+            .trim()
+            .bytes()
+            .enumerate()
+            .flat_map(|(i, b)| {
+                let value = if i % 2 == 0 {
+                    id += 1;
+                    id - 1
+                } else {
+                    -1
+                };
+                (0..b - b'0').map(move |_| (1, value))
+            })
+            .collect(),
+    )
+}
+
+fn part_2(input: &str) -> usize {
+    let mut id = 0;
+    compact_disk(
+        input
+            .trim()
+            .bytes()
+            .enumerate()
+            .map(|(i, b)| {
+                let value = if i % 2 == 0 {
+                    id += 1;
+                    id - 1
+                } else {
+                    -1
+                };
+                ((b - b'0') as usize, value)
+            })
+            .collect(),
+    )
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    static INPUT: &str = "";
-    static INPUT_2: &str = "";
+    static INPUT: &str = "2333133121414131402";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(INPUT), 0);
-        assert_eq!(part_1(INPUT_TXT), 0);
+        assert_eq!(part_1(INPUT), 1928);
+        assert_eq!(part_1(INPUT_TXT), 6323641412437);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(INPUT_2), 0);
-        assert_eq!(part_2(INPUT_TXT), 0);
+        assert_eq!(part_2(INPUT), 2858);
+        assert_eq!(part_2(INPUT_TXT), 6351801932670);
     }
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Solutions for [adventofcode.com](https://adventofcode.com)
 
  <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2024 - 16 ⭐
+  2024 - 18 ⭐
 </h1>
 <a href="2024/src/bin/01.rs">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -29,6 +29,9 @@ Solutions for [adventofcode.com](https://adventofcode.com)
 </a>
 <a href="2024/src/bin/08.rs">
   <img src=".aoc_tiles/tiles/2024/08.png" width="161px">
+</a>
+<a href="2024/src/bin/09.rs">
+  <img src=".aoc_tiles/tiles/2024/09.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐
