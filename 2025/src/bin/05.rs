25f2f3a9345120c0735a2d978131869b3c8f4bf1
Cian Butler <butlerx@notthe.cloud>
2025-12-05 05:19:19 +0000

    2025: day 5 solution

--- 2025/src/bin/05.rs
+++ 2025/src/bin/05.rs
@@ -1,41 +1,89 @@
-#![warn(clippy::pedantic)]
-#![allow(clippy::missing_panics_doc)]
-
-use aoc_shared::time_execution;
+use aoc_shared::time_execution_us;
 static INPUT_TXT: &str = include_str!("../../input/05.txt");
 
 fn main() {
     println!("🌟 --- Day 5 Results --- 🌟");
-    let (res_1, duration_1) = time_execution(|| part_1(INPUT_TXT));
-    println!("📌 Part 1: {res_1}, complete in {duration_1} ms");
+    let (res_1, duration_1) = time_execution_us(|| part_1(INPUT_TXT));
+    println!("📌 Part 1: {res_1}, complete in {duration_1} us");
 
-    let (res_2, duration_2) = time_execution(|| part_2(INPUT_TXT));
-    println!("📌 Part 2: {res_2}, complete in {duration_2} ms");
+    let (res_2, duration_2) = time_execution_us(|| part_2(INPUT_TXT));
+    println!("📌 Part 2: {res_2}, complete in {duration_2} us");
 }
 
-fn part_1(_input: &str) -> usize {
-    0
+#[inline]
+fn parse_range(section: &str) -> (usize, usize) {
+    let (start_str, end_str) = section.split_once('-').expect("Invalid range");
+    let start = start_str.parse::<usize>().expect("Invalid start number");
+    let end = end_str.parse::<usize>().expect("Invalid end number");
+    (start, end)
 }
 
-fn part_2(_input: &str) -> usize {
-    0
+fn part_1(input: &str) -> usize {
+    let (ranges, ingredient) = input.trim().split_once("\n\n").expect("Invalid input");
+
+    let fresh_ranges: Vec<(usize, usize)> = ranges.trim().lines().map(parse_range).collect();
+
+    ingredient
+        .trim()
+        .lines()
+        .map(|line| line.parse::<usize>().expect("Invalid number"))
+        .filter(|&n| {
+            fresh_ranges
+                .iter()
+                .any(|(start, end)| n >= *start && n <= *end)
+        })
+        .count()
+}
+
+fn part_2(input: &str) -> usize {
+    let (ranges, _) = input.trim().split_once("\n\n").expect("Invalid input");
+
+    let mut ranges: Vec<(usize, usize)> = ranges.trim().lines().map(parse_range).collect();
+    ranges.sort_unstable_by_key(|r| r.0);
+
+    ranges
+        .into_iter()
+        .fold::<Vec<(usize, usize)>, _>(vec![], |mut merged, (start, end)| {
+            match merged.last_mut() {
+                Some(last) if start <= last.1 + 1 => {
+                    last.1 = last.1.max(end);
+                }
+                _ => {
+                    merged.push((start, end));
+                }
+            }
+            merged
+        })
+        .iter()
+        .map(|(start, end)| end - start + 1)
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part_1(TEST_INPUT), 0);
-        assert_eq!(part_1(INPUT_TXT), 0);
+        assert_eq!(part_1(TEST_INPUT), 3);
+        assert_eq!(part_1(INPUT_TXT), 520);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part_2(TEST_INPUT), 0);
-        assert_eq!(part_2(INPUT_TXT), 0);
+        assert_eq!(part_2(TEST_INPUT), 14);
+        assert_eq!(part_2(INPUT_TXT), 347_338_785_050_515);
     }
 }
